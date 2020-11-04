8800d4278b314185fdc64fbae7c09b699227ec0c
张辰磊 <1443985700@qq.com>
2020-11-04 21:14:10 +0800

    第一次提交config

--- config/src/main/java/com/jk/ConfigApplication.java
+++ config/src/main/java/com/jk/ConfigApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class DemoApplication {
+public class ConfigApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(ConfigApplication.class, args);
     }
 
 }
